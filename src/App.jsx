8c3dd01ea7a3b75fac74b205b942d4e0c2068717
FrancescoGiommi <francescogiommi97@icloud.com>
2025-01-01 18:37:58 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import Header from "./components/Header";
+import Main from "./components/Main";
+function App() {
+  return (
+    <>
+      <div className="container my-3 text-center">
+        <Header />
+        <Main />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <>
+      <h1>Tombola</h1>
+    </>
+  );
+}
