9c3b6faf7df258fb68380ef3e492f1aa09cc9a2f
FrancescoGiommi <francescogiommi97@icloud.com>
2025-01-02 19:01:21 +0100

    Add use state to see number extracted

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,19 +1,29 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 export default function Main() {
-  const numbers = [
+  const numbersToBeExtracted = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
     22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
     41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
     60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78,
     79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
   ];
+  const [numbers, seNumbers] = useState(numbersToBeExtracted);
+  const [currentNumber, setCurrentNumber] = useState(null);
 
-  /* Estraggo il numero dall'array */
+  /* Estraggo un numero dall'array */
   const handleExtractNumber = () => {
-    numbers.splice(Math.floor(Math.random() * numbers.length), 1);
-    console.log(numbers);
-  };
+    if (numbers.length === 0) return alert("Hai estratto tutti i numeri!");
 
-  console.log(numbers);
+    const randomNumber = Math.floor(Math.random() * numbers.length);
+    const newNumber = [...numbers];
+    const extractNumber = newNumber.splice(randomNumber, 1)[0];
+    seNumbers(newNumber);
+    setCurrentNumber(extractNumber);
+    console.log(newNumber);
+    console.log(`Hai estratto il numero ${extractNumber}`);
+  };
 
   return (
     <>
@@ -24,7 +34,7 @@ export default function Main() {
       </div>
       <div style={{ maxWidth: "320px" }}>
         <div>Ultimo numero estratto</div>
-        <div className="extract-number"></div>
+        <div className="extract-number">{currentNumber}</div>
         <hr />
         <div className="d-flex flex-column gap-2">
           <button className="btn btn-warning" onClick={handleExtractNumber}>
