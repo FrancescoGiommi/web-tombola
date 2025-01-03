dee9864c421a5d52888716a85fbf2425eb6c8d46
FrancescoGiommi <francescogiommi97@icloud.com>
2025-01-03 17:47:19 +0100

    Add extractedNumbers and text-bg-danger on class

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,7 @@
-import { useEffect } from "react";
 import { useState } from "react";
 
 export default function Main() {
+  /* Numeri da estrarre */
   const numbersToBeExtracted = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
     22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
@@ -9,27 +9,41 @@ export default function Main() {
     60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78,
     79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
   ];
-  const [numbers, seNumbers] = useState(numbersToBeExtracted);
+
+  const [numbers] = useState(numbersToBeExtracted);
   const [currentNumber, setCurrentNumber] = useState(null);
+  const [extractedNumbers, setExtractedNumbers] = useState([]);
 
-  /* Estraggo un numero dall'array */
+  /* Funzione per estrarre i numeri */
   const handleExtractNumber = () => {
-    if (numbers.length === 0) return alert("Hai estratto tutti i numeri!");
+    /* Controllo se i numeri sono usciti tutti */
+    if (extractedNumbers.length === numbers.length) {
+      return alert("Hai estratto tutti i numeri!");
+    }
 
-    const randomNumber = Math.floor(Math.random() * numbers.length);
-    const newNumber = [...numbers];
-    const extractNumber = newNumber.splice(randomNumber, 1)[0];
-    seNumbers(newNumber);
-    setCurrentNumber(extractNumber);
-    console.log(newNumber);
-    console.log(`Hai estratto il numero ${extractNumber}`);
+    let randomNumber;
+    do {
+      randomNumber = Math.floor(Math.random() * numbers.length);
+    } while (extractedNumbers.includes(numbers[randomNumber]));
+
+    const extractedNumber = numbers[randomNumber];
+    setExtractedNumbers([...extractedNumbers, extractedNumber]);
+    setCurrentNumber(extractedNumber);
+    console.log(`Hai estratto il numero ${extractedNumber}`);
   };
 
   return (
     <>
       <div className="row d-flex" style={{ maxWidth: "1000px" }}>
-        {numbers.map((number) => (
-          <div className="numbers col-2">{number}</div>
+        {numbers.map((number, index) => (
+          <div
+            key={index}
+            className={`numbers col-2 ${
+              extractedNumbers.includes(number) ? "text-bg-danger" : ""
+            }`}
+          >
+            {number}
+          </div>
         ))}
       </div>
       <div style={{ maxWidth: "320px" }}>
@@ -40,7 +54,12 @@ export default function Main() {
           <button className="btn btn-warning" onClick={handleExtractNumber}>
             Estrai
           </button>
-          <button className="btn btn-danger">Termina gioco</button>
+          <button
+            className="btn btn-danger"
+            onClick={() => alert("Gioco terminato!")}
+          >
+            Termina gioco
+          </button>
         </div>
       </div>
     </>
